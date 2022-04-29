cd2ccc63e5a5eeb29f21d1b13e5a556317ad5950
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-29 16:21:11 +0600

    Updated mongodb username password

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://GroInventory:<password>@cluster0.d5bm4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d5bm4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
