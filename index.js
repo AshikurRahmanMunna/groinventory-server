bb16f9417f1539bcdac7c0e24c758ec8b01fb15c
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-30 22:36:49 +0600

    Added reviews api

--- index.js
+++ index.js
@@ -9,22 +9,45 @@ app.use(cors());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d5bm4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         const itemsCollection = client.db('GroInventory').collection('items');
+        const reviewsCollection = client.db('GroInventory').collection('reviews');
         console.log('Database connected');
+        // get all items
         app.get('/items', async (req, res) => {
             const cursor = itemsCollection.find({});
             const result = await cursor.toArray();
             res.send(result);
         })
+        // add an item
         app.post('/items', async (req, res) => {
             const item = req.body;
             const result = await itemsCollection.insertOne(item);
+            res.send(result);
+        })
+        // delete an item
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await itemsCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+        // get all reviews
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // add a review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
             res.send(result);
         })
     }
